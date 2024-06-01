dd9ba9f5c3948550cd2605b6b6d74f181bf414c6
Tikiu04 <trantu8894@gmail.com>
2024-06-01 13:38:35 +0900

    bai tap 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mang.......");
+            Console.WriteLine("Nhap vao so phan tu cua mang : ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = i + 100;
+                Console.WriteLine("phan tu {0} co gia tri la {1}", i, arr[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
