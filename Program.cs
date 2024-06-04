cb3e5f970536792b6aa4775fb894f73d948b387e
Tikiu04 <139756458+Tikiu04@users.noreply.github.com>
2024-06-04 09:38:12 +0900

    20240604.2

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(" Hãy nhập Id từ bàn phím : ");
+            var id = Console.ReadLine();
+            Console.WriteLine(" Hãy nhập tên từ bàn  phím : ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hãy nhập địa chỉ từ bàn phím : ");
+            var add = Console.ReadLine();
+            Console.WriteLine("Hãy nhập ngày sinh từ bàn phím : ");
+            var ngaysinh = Console.ReadLine();
+            Console.WriteLine("Hãy nhập lương từ bàn phím :");
+            float luong = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hãy nhập thưởng từ bàn phím :");
+            float thuong = float.Parse(Console.ReadLine());
+            float tongluong = luong + thuong;
+            Console.WriteLine("Thông tin nhân viên :");
+            Console.WriteLine("Mã Id : " + id);
+            Console.WriteLine("Họ và tên : " + name);
+            Console.WriteLine("Địa chỉ : " + add);
+            Console.WriteLine("Ngày sinh : " +  ngaysinh);
+            Console.WriteLine("Lương : " + luong);
+            Console.WriteLine("Thưởng : " + thuong );
+            Console.WriteLine("Tổng  lương : "+ tongluong);
+        }
+    }
+}
